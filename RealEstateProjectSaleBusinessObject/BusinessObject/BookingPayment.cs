9eac5da9c68b87370d8a999639bfee97e659ee65
Duy1408 <duynguyen08142001@gmail.com>
2024-06-20 07:51:50 +0700

    DB-Context

--- RealEstateProjectSaleBusinessObject/BusinessObject/BookingPayment.cs
+++ RealEstateProjectSaleBusinessObject/BusinessObject/BookingPayment.cs
@@ -9,17 +9,13 @@ namespace RealEstateProjectSaleBusinessObject.BusinessObject
     public class BookingPayment
     {
         public Guid BookingPaymentID { get; set; }
-        public Guid CustomerID { get; set; }
-       
+        public Guid CustomerID { get; set; }     
         public Guid ProjectID { get; set; }
         public int Deposittoholdproject { get; set; }
         public bool Status { get; set; }
-
         public Guid PaymentTypeID { get; set; }
         public PaymentType PaymentType { get; set; }
         public Guid BookingID { get; set; }
         public Booking Booking { get; set; }
-
-
     }
 }
